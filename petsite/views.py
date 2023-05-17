352d96cdc2a3f66358ff6ff4af476d5477b00296
ITKOO <itkoo2000@gmail.com>
2023-05-17 12:42:30 +0900

    feat(main): Message DB 조회 후 출력

--- petsite/views.py
+++ petsite/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+from .models import Message
+
+
 # Create your views here.
+
+def menu_list(request):
+    messages = Message.objects.order_by('created_date')
+    return render(request, 'menu/menu_list.html', {'messages': messages})
