6d0f31431b6666d8f7e0eeb6b5aa3402fcd387db
ITKOO <itkoo2000@gmail.com>
2023-05-17 13:59:01 +0900

    feat(main): 어드민에 각 모델 등록

--- petsite/admin.py
+++ petsite/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Message
+from .models import Message, Snack, Gallery
 
 admin.site.register(Message)
+admin.site.register(Snack)
+admin.site.register(Gallery)
