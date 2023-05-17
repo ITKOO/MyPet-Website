f1b93080aa78cf77d5f2cff71e1ec89af239d5a6
ITKOO <itkoo2000@gmail.com>
2023-05-17 13:14:17 +0900

    feat(main): 템플릿 확장(base.html)

--- petsite/urls.py
+++ petsite/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.menu_list, name='menu_list'),
+    path('message', views.message_list, name='message_list'),
 ]
--- petsite/views.py
+++ petsite/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 from .models import Message
 
 
-# Create your views here.
-
 def menu_list(request):
+    return render(request, 'menu/menu_list.html', {})
+
+
+def message_list(request):
     messages = Message.objects.order_by('created_date')
-    return render(request, 'menu/menu_list.html', {'messages': messages})
+    return render(request, 'message/message_list.html', {'messages': messages})
